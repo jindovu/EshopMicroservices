e653b14457eb0cd8f23d7a41c04496b7e5e83880
vth1 <vth1@mascot.dk>
2025-04-12 17:27:28 +0700

    Implement Handle Validator behavior

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,18 @@
 
     public record UpdateProductResult(bool IsSuccess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name Id is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
@@ -14,7 +26,7 @@
             logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
 
             var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
-            if(product is null)
+            if (product is null)
             {
                 throw new ProductNotFoundException();
             }
@@ -24,7 +36,7 @@
             product.Description = command.Description;
             product.ImageFile = command.ImageFile;
             product.Price = command.Price;
-            
+
             session.Update(product);
             await session.SaveChangesAsync();
 
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,15 +1,18 @@
-using Carter;
-using Marten;
+using BuildingBlocks.Behaviors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
